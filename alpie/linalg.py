55317fc3da62177e024d44160cb1b241c87adbb9
Paul Lynn <lynnporu@gmail.com>
2019-09-27 20:13:22 +0300

    Add `shape` and `dimensions` functions

--- alpie/linalg.py
+++ alpie/linalg.py
@@ -14,11 +14,73 @@ class MultidimensionalMatrix:
         else:
             raise ValueError("Check docstring for help.")
 
+    def shape(
+        self, fillwith=None, dimensions=None, unpack=False,
+        background=None
+    ):
+        """If unpack==False, then matrix will be filled with copies of
+        `fillwith` object.
+        If unpack==True, then elements of `fillwith` will be shaped into matrix
+        with given dimensions in recursive order. Missing elements will be
+        filled as `background`, otherwise ValueError will raise.
+        """
+        self.data = list()
+
+        def fill(array, size, *coords):
+
+            for _ in range(size):
+
+                # We're at the lowest level.
+                if not coords:
+                    if not unpack:
+                        array.append(deepcopy(fillwith))
+                    else:
+                        if not fillwith:
+                            array.append(deepcopy(background))
+                        else:
+                            array.append(fillwith.pop(0))
+
+                # Unpack coords list to size + coords
+                else:
+                    item = list()
+                    fill(item, *coords)
+                    array.append(item)
+
+        fill(self.data, *dimensions)
+
+        return self
+
     @classmethod
     def empty(cls):
         """Creates empty matrix.
         """
         return cls(data=[])
+
+    @property
+    def dimensions(self):
+        """Measure dimensions of data.
+        """
+        coords = list()
+
+        def measure(array, depth=0):
+
+            if type(array) is not list:
+                return
+
+            # Check if coords list are big enough.
+            if len(coords) - 1 < depth:
+                coords.append(0)
+
+            # Add size of the biggest element at this dimension.
+            coords[depth] = max(coords[depth], len(array))
+
+            # Go recursively.
+            for item in array:
+                measure(item, depth + 1)
+
+        measure(self.data)
+
+        return coords
 
     def elements(self):
         """Generates elements of matrix in the recursive order.
