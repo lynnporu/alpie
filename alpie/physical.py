0c30b54c7915df02cd18251698535ba739abada7
Paul Lynn <lynnporu@gmail.com>
2019-11-03 16:01:08 +0200

    Improved physical
    * Described interface of Space: __contains__, size, __iter__ and __len__ methods;
    * Calculate __len__ as self.size / self.detalization;
    * Add closed and opened hyperballs.

--- alpie/physical.py
+++ alpie/physical.py
@@ -5,6 +5,7 @@
 import itertools
 import functools
 import operator
+import math
 
 
 def frange(start, stop, step):
@@ -19,7 +20,20 @@ def frange(start, stop, step):
 
 
 class Space():
-    pass
+
+    def __len__(self):
+        """Get number of discrete points in this figure.
+        """
+        return self.size / self.detalization
+
+    def __iter__(self):
+        raise NotImplemented
+
+    def __contains__(self):
+        raise NotImplemented
+
+    def size(self):
+        raise NotImplemented
 
 
 class Rectangle(Space):
@@ -40,26 +54,23 @@ class Rectangle(Space):
             for a, b
             in zip(self.start, self.end)])
 
-    def __len__(self):
-        """Get number of discrete points in this figure.
+    def size(self):
+        """Get continuum size of figure.
         """
-        return int(
-            functools.reduce(
-                operator.mul,
-                [
-                    a - b
-                    for a, b
-                    in zip(self.end, self.start)
-                ]
-            ) / self.detalization
-        )
+        return functools.reduce(
+            # Multiply sizes
+            operator.mul, [
+                # Calculate sizes of the figure.
+                (a - b)
+                for a, b
+                in zip(self.start, self.end)])
 
 
 class Square(Rectangle):
     """A multidimensional square.
     """
 
-    def __init__(self, start: tuple, size: int, detalization=1e-3):
+    def __init__(self, start: tuple, size: float, detalization=1e-3):
         """Creates square with coordinates:
         (start[0], start[1], ..., start[dim]) ->
             (start[0] + size, start[1] + size, ..., start[dim] + size)
@@ -84,7 +95,47 @@ class Range(Space):
     def __iter__(self):
         return frange(self.start, self.end, self.detalization)
 
-    def __len__(self):
-        """Get number of discrete points in this figure.
+    def size(self):
+        """Continuum size of the range.
         """
-        return abs(self.start - self.end) / self.detalization
+        return abs(self.start - self.end)
+
+
+class ClosedHyperball(Space):
+    """A multidimensional closed ball.
+    """
+
+    def __init__(self, center: tuple, radius: float, detalization=1e-3):
+        """Creates ball with center in given coordinates. Your ball will have
+        dimension equal to len(center).
+        """
+        self.start = center
+        self.radius = radius
+        self.detalization = detalization
+
+    def __iter__(self):
+        return [
+            point
+            for point
+            # TODO: Not efficient. Replace with some trigonometry.
+            in Square(self.start, self.radius, self.detalization)
+            if point in self
+        ]
+
+    def __contains__(self, point: tuple):
+        return sum(x ** 2 for x in point) <= self.radius ** 2
+
+    def size(self):
+        """Continuum size of your hyperball.
+        """
+        return (
+            (math.pi ** (len(self.size) / 2)) / math.gamma(len(self.size) + 1)
+        ) * self.radius ** len(self.size)
+
+
+class OpenedHyperball(Space):
+    """A multidimensional opened ball.
+    """
+
+    def __contains__(self, point: tuple):
+        return sum(x ** 2 for x in point) < self.radius ** 2
